970eb6c832468c5bcf76ebcc1c1b46b0bfba967c
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-09-22 12:59:10 +0200

    cleanup: remove more old stuff

--- src/Products/PloneKeywordManager/__init__.py
+++ src/Products/PloneKeywordManager/__init__.py
@@ -17,10 +17,6 @@
 # http://trific.ath.cx/resources/python/levenshtein/
 ##
 ###
-from AccessControl.Permission import registerPermissions
-from Products.CMFCore.utils import ToolInit
-from Products.PloneKeywordManager import config
-from Products.PloneKeywordManager import tool
 from zope.i18nmessageid import MessageFactory
 
 import logging
@@ -28,16 +24,3 @@ import logging
 
 keywordmanagerMessageFactory = MessageFactory("Products.PloneKeywordManager")
 logger = logging.getLogger("Products.PloneKeywordManager")
-
-
-registerPermissions(
-    [(config.MANAGE_KEYWORDS_PERMISSION, [])], ("Manager", "Site Administrator")
-)
-
-
-def initialize(context):
-
-    new_tool = ToolInit(
-        config.TOOL_NAME, tools=(tool.PloneKeywordManager,), icon="tool.gif"
-    )
-    new_tool.initialize(context)
