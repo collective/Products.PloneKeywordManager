969d98e75799b2d612a52dfeb9fefc0b0fcd9198
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-09-22 12:51:44 +0200

    cleanup: remove unneeded code
    Plone 5.2.0 is using p.a.testing 6.x already

--- src/Products/PloneKeywordManager/tests/base.py
+++ src/Products/PloneKeywordManager/tests/base.py
@@ -1,9 +1,7 @@
 from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
-from Products.PloneKeywordManager.browser.interfaces import IPloneKeywordManagerLayer
 from Products.PloneKeywordManager.testing import PLONEKEYWORDMANAGER_INTEGRATION_TESTING
-from zope import interface
 from zope.component import getMultiAdapter
 
 import unittest
@@ -16,16 +14,8 @@ class BaseIntegrationTestCase(unittest.TestCase):
     def setUp(self):
         self.portal = self.layer["portal"]
         self.request = self.layer["request"]
-        self.markRequestWithLayer()
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
         self.pkm = api.portal.get_tool("portal_keyword_manager")
-
-    def markRequestWithLayer(self):
-        # to be removed when p.a.testing will fix https://dev.plone.org/ticket/11673
-        # Well, that's a 404, but p.a.testing 4.2.2 -> 4.2.4 has a release note that it was fixed
-        #  Plone 4.3.3 release notes.
-        request = self.layer["request"]
-        interface.alsoProvides(request, IPloneKeywordManagerLayer)
 
 
 class PKMTestCase(BaseIntegrationTestCase):
